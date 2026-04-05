02e027b79054bc9865a35c54636cee536a9b5e88
Vinicinn <viniciusmelo554@gmail.com>
2026-04-05 13:25:17 -0300

    implementacao pagina principal estatica com os conteudos do usuario

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,32 @@
-import { Link } from "react-router-dom";
+import "../styles/home.css";
 
 export function Home() {
+  const today = new Date().toLocaleDateString("pt-BR", {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
   return (
-    <div>
-      <h1>Dashboard de Estudos</h1>
-      <Link to="/study">Registrar Novo Estudo</Link>
-      {/*listar estudos, revisões, etc. */}
+    <div className="home-page">
+      <div className="home-header">
+        <h1>Olá, usuario</h1>
+        <span>{today}</span>
+      </div>
+      <div className="home-window">
+        <div className="home-card">
+          <p className="home-card-title">Meus conteúdos</p>
+          <p className="home-empty">Nenhum conteúdo cadastrado.</p>
+        </div>
+        <div className="home-card">
+          <p className="home-card-title">Recomendação de Revisões</p>
+          <p className="home-empty">Nenhuma recomendação cadastrado.</p>
+        </div>
+        <div className="home-card">
+          <p className="home-card-title">Agendamentos</p>
+          <p className="home-empty">Nenhum agendamento cadastrado.</p>
+        </div>
+      </div>
     </div>
   );
 }
