9c40439dbb2fba0d3ccb2fb177ff1a549da946b3
Vinicinn <viniciusmelo554@gmail.com>
2026-04-20 20:45:07 -0300

    adicao da integracao com o GROQ

---
+++ backend/config/groq.js
@@ -0,0 +1,25 @@
+import { Groq } from "groq-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+export async function getResponse(prompt) {
+  const chatCompletion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "llama-3.1-8b-instant",
+    temperature: 1,
+    max_completion_tokens: 1024,
+    top_p: 1,
+    stream: false,
+    stop: null,
+  });
+
+  return chatCompletion.choices[0].message.content;
+}
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 dotenv.config();
 
 import { connectToDatabase, client } from "./config/db.js";
+import { getResponse } from "./config/groq.js";
 import { userRoutes } from "./routes/userRoutes.js";
 import { contentRoutes } from "./routes/contentRoutes.js";
 import { reviewRoutes } from "./routes/reviewRoutes.js";
@@ -22,20 +23,20 @@ import { ReviewRepository } from "./repositories/reviewRepository.js";
 await connectToDatabase();
 const db = client.db("gestaoEstudos");
 
-// instanciando camadas de usuario
-const userRepository = new UserRepository(db);
-const userService = new UserService(userRepository);
-const userController = new UserController(userService);
+// instanciando camadas de revisoes
+const reviewRepository = new ReviewRepository(db);
+const reviewService = new ReviewService(reviewRepository);
+const reviewController = new ReviewController(reviewService);
 
 // instanciando camadas de conteudos
 const contentRepository = new ContentRepository(db);
 const contentService = new ContentService(contentRepository);
 const contentController = new ContentController(contentService);
 
-// instanciando camadas de revisoes
-const reviewRepository = new ReviewRepository(db);
-const reviewService = new ReviewService(reviewRepository);
-const reviewController = new ReviewController(reviewService);
+// instanciando camadas de usuario
+const userRepository = new UserRepository(db);
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
 
 const app = express();
 app.use(cors());
