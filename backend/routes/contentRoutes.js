5ab0e6d597325356a2b8a45355306d70e86331b5
Vinicinn <viniciusmelo554@gmail.com>
2026-04-23 14:25:16 -0300

    correcao de rotas apagadas

--- backend/routes/contentRoutes.js
+++ backend/routes/contentRoutes.js
@@ -7,11 +7,13 @@ export function contentRoutes(contentController) {
   router.get("/user/:id", (req, res) => contentController.getAllUserContents(req, res));                      // envia todos os conteudos de um usuario (ok)
   router.get("/:id", (req, res) => contentController.getContentById(req, res));                               // envia o conteudo com o id (ok)
   router.get("/user/:id/recommendations", (req, res) => contentController.getUserRecommendations(req, res));  // envia as recomendacoes de conteudo de um usuario 
+  router.get("/:id/review-info", (req, res) => contentController.getReviewDatesInfo(req, res));
   
   router.post("/", (req, res) => contentController.createContent(req, res));                                  // cria um novo conteudo (ok)
   router.post("/:id/feedback", (req, res) => contentController.submitContentFeedback(req, res));              // atualiza as metricas de um conteudo
   
   router.put("/:id", (req, res) => contentController.updateContent(req, res));                                // atualiza conteudo pelo id (ok)
+  router.put("/:id/review-dates", (req, res) => contentController.updateReviewDates(req, res));
   
   router.delete("/all", (req,res) => contentController.deleteAllContents(req,res));                           // deletar todos os conteudos (USAR EM PROD, APAGAR DPS) (ok)
   router.delete("/:id", (req, res) => contentController.deleteContent(req, res));                             // apaga um conteudo pelo id (ok)
